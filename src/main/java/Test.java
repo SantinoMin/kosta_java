0b560af6e6e489ec42be24100cff57dc00088889
SantinoMin <santinomin0381@gmail.com>
2024-08-13 19:24:37 +0900

    feat : coding test.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,31 +1,40 @@
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Test {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
 
-//    문제 10950번
-//    두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//    문제
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//
+//입력
+//입력은 여러 개의 테스트 케이스로 이루어져 있다.
+//
+//각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
 
-//    입력
-//    첫째 줄에 테스트 케이스의 개수 T가 주어진다.
-//    각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str;
 
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    sc.nextLine();
+    while( (str=br.readLine()) != null) {
+      StringTokenizer st = new StringTokenizer(str);
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
 
-    for (int i = 0; i < N; i++) {
-
-      String input = sc.nextLine();
-      String[] numbers = input.split(" ");
-
-      int a = Integer.parseInt(numbers[0]);
-      int b = Integer.parseInt(numbers[1]);
-
-      System.out.println(a + b);
+      System.out.println(a+b);
     }
-    sc.close();
+    br.close();
   }
 };
+
+
+//    String input = sc.nextLine();
+//    String[] arr = input.split(" ");
+//    String sum = arr[0] + arr[1];
+//
+//    for(int i = 0; i < 5; i++){
+//      System.out.println(a+b);
+//      sc.nextLine();
+
