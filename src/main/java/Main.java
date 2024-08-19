857b0a6039b31175a11178902b4ec8160180214f
SantinoMin <santinomin0381@gmail.com>
2024-08-19 19:44:51 +0900

    feat : coding test.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,52 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
 
-    // 우선 모든 값을 배열에 넣어야지 넣은 값중에 가장 짧은걸 찾을 수 있을듯?
 
-    // 입력을 내가 하는게 아니라, 원래 배열에 넣어둬야 되는건가?
-    // -> 단어의 개수 N이 주어지기 때문에, N값은 정해져 있는게 아니라, N개의 줄에 단어들이 있는걸 보면, N개의 줄에 있는 값들의 합을
-    // N = 단어들 수의 합
+    //1.첫째줄에는 단어의 개수 N개가 출력됨
+    //2.길이 비교하기 (짧은 것부터, 길이가 같으면 사전 순으로)
+    //3.데이터를 ArrayList에 넣어서, 비교를 해야 될 듯?
 
-    // 값이 어떤식으로 구성되어 있는지부터 생각해보기
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String N = br.readLine();
 
-    Scanner sc = new Scanner(System.in);
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.add(br.readLine());
 
-    // 0. 첫 줄에는 배열에 넣은 값들, 배열 - 1개
-    // 1. 값들을 배열에 넣고, 그 안에서 비교를 해보기. -- 배열의 0번째 vs 배열의 1번째 비교하는 메서드로.
-    // 2. 길이가 짧은 순이 앞으로 오도록
-    // 3. 길이가 같다면, 사전 순으로.
 
-    int count = sc.nextInt();
-    ArrayList<String> array = new ArrayList<>();
-    sc.nextLine();
 
-    for (int i = 0; i < count - 1; i++) {
-      array.add(sc.nextLine());
-    }
-    array.remove(array.get(0));
 
-    System.out.println(array);
 
-//     array[n]과 array[n+1]을 비교해서, 1) 만약 값이 같다면, n+1번째 값을 삭제.
-//     array[n]과 array[n+1]의 문자열 길이를 비교해서 array[n]의 문자열 길이가 짧다면, 그대로 진행
-//     but array[n+1]의 문자열 길이가 짧다면, array[n] = array[n+1]로 진행 +
 
-    for(int i=0; i < array.size(); i++) {
 
-      if (array.size() > 1) {
-          if(array.get(i).length() > array.get(i+1).length()){
-            array.set(i, array.get(i+1));
-          } if(array.get(i) == array.get(i+1)){
-            array.remove(array.get(i+1));
-        }
-
-      }
-
-    }
   }
 }
 
